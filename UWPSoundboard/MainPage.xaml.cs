273bbd5ccb06e73bccdee72ec8c62f73a76f13e1
jono- <johnmannion51@gmail.com>
2018-04-13 00:15:13 +0100

    Add project files.

---
+++ UWPSoundboard/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPSoundboard.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPSoundboard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Sound> Sounds;
+
+        private List<MenuItem> MenuItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Sounds = new ObservableCollection<Sound>();
+            SoundManager.GetAllSounds(Sounds);
+            MenuItems = new List<MenuItem>();
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/teams.png", Category = SoundCategory.Teams });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/taunt.png", Category = SoundCategory.Chants });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/manager.png", Category = SoundCategory.Managers });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/fans.png", Category = SoundCategory.Other });
+
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundManager.GetAllSounds(Sounds);
+            CategoryTextBlock.Text = "All Sounds";
+            MenuItemsListView.SelectedItem = null;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+
+            // Filter on category
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            SoundManager.GetSoundsByCategory(Sounds, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
+        }
+
+        private void SoungGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var sound = (Sound)e.ClickedItem;
+            MyMediaElement.Source = new Uri(this.BaseUri, sound.AudioFile);
+
+        }
+    }
+}
---
+++ UWPSoundboard/Model/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundboard.Model
+{
+    public class SoundManager
+    {
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+            allSounds.ForEach(p => sounds.Add(p));
+        }
+
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCategory)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == soundCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+            //Teams category
+            sounds.Add(new Sound("Blue", SoundCategory.Teams));
+            sounds.Add(new Sound("Bubbles", SoundCategory.Teams));
+            sounds.Add(new Sound("Walk", SoundCategory.Teams));
+            sounds.Add(new Sound("Glory", SoundCategory.Teams));
+            sounds.Add(new Sound("Spurs", SoundCategory.Teams));
+            //Chants category
+            //sounds.Add(new Sound("Foot", SoundCategory.Chants));
+            sounds.Add(new Sound("Viva", SoundCategory.Chants));
+            sounds.Add(new Sound("Who", SoundCategory.Chants));
+            sounds.Add(new Sound("Lose", SoundCategory.Chants));
+            sounds.Add(new Sound("Pretend", SoundCategory.Chants));
+            sounds.Add(new Sound("Goal", SoundCategory.Chants));
+
+            //Managers category
+            sounds.Add(new Sound("Jose", SoundCategory.Managers));
+            sounds.Add(new Sound("Klopp", SoundCategory.Managers));
+            sounds.Add(new Sound("Pep", SoundCategory.Managers));
+            sounds.Add(new Sound("W", SoundCategory.Managers));
+            //Random category
+            sounds.Add(new Sound("Charge", SoundCategory.Other));
+            sounds.Add(new Sound("Siren", SoundCategory.Other));
+            sounds.Add(new Sound("Scores", SoundCategory.Other));
+            sounds.Add(new Sound("Rumble", SoundCategory.Other));
+            sounds.Add(new Sound("Foot", SoundCategory.Other));
+
+            return sounds;
+        }
+    }
+}
